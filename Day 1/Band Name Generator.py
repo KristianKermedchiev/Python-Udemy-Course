4c672551abb5d012eb52460417eb60db91674369
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-08-05 17:03:54 +0300

    Day 1 final exercise

---
+++ Day 1/Band Name Generator.py
@@ -0,0 +1,15 @@
+# Attempt number 1
+print('Welcome to the band name generator!')
+city = input('Which city did you grow up in?\n')
+pet = input('What is the name of a pet?\n')
+print("Your band name could be " + city + " " + pet)
+
+
+
+# Attempt number 2
+
+# firstName = input('What is the name of the city you wer born in?')
+# lastName = input('What is your first pet\'s name?')
+# print(firstName + " " + lastName)
+
+
