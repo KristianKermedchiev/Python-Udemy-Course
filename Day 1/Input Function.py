25ff7bf89df655c0158ba97253d5ed6d0ae21299
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-08-05 16:35:05 +0300

    Day 1 Exercises

---
+++ Day 1/Input Function.py
@@ -0,0 +1,11 @@
+# Instructions
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+#
+#
+# Example Input
+# Angela
+# Example Output
+# 6
+
+user_input = input('What is your name? ')
+print(len(user_input))
