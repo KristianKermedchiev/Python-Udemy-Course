83da816a591877c1c6bf89c21974e4f3b4a6d73f
Kristian Kermedchiev <k.kermedchiev@gmail.com>
2024-06-26 23:13:20 +0300

    adding high-score functionality to the game

--- Day 20 and 21 - Animation, Coordinates, Inheritence and List Slicing - Building the Snake Game/main.py
+++ Day 20 and 21 - Animation, Coordinates, Inheritence and List Slicing - Building the Snake Game/main.py
@@ -27,22 +27,23 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with food.
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         scoreboard.increase_score()
         snake.extend()
         food.refresh()
 
-    #Detect collision with wall.
+    # Detect collision with wall.
     if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() < -295 or snake.head.ycor() > 295:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
-    #Detect collision with self.
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+    # Detect collision with self.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
--- Day 20 and 21 - Animation, Coordinates, Inheritence and List Slicing - Building the Snake Game/score.py
+++ Day 20 and 21 - Animation, Coordinates, Inheritence and List Slicing - Building the Snake Game/score.py
@@ -9,6 +9,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color('white')
         self.penup()
         self.hideturtle()
@@ -16,13 +18,17 @@ class Score(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", move=MOVE, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=MOVE, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", move=MOVE, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
