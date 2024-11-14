4d08a729faab429914c814422c20853de3648eb8
Kristian Kermedchiev <k.kermedchiev@gmail.com>
2024-11-14 20:23:11 +0200

    Push

---
+++ Day 25 - Working with CSV Data and the Pandas Library/main.py
@@ -0,0 +1,16 @@
+# with open("weather_data.csv") as csv_file:
+#     data = csv_file.readlines()
+# print(data)
+
+# import csv
+#
+# with open('weather_data.csv') as csvfile:
+#     reader = csv.reader(csvfile)
+#     temperatures = []
+#     for row in reader:
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+#
+# print(temperatures)
+
+import pandas as pd:
